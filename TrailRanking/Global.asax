4a0fd733f8e71cdfb990d139b6d8a01d13c59d9e
dcass78 <dcass78@yahoo.com>
2018-11-28 10:37:17 -0500

    Add project files.

---
+++ TrailRanking/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrailRanking.MvcApplication" Language="C#" %>
---
+++ TrailRanking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrailRanking.Startup))]
+namespace TrailRanking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
